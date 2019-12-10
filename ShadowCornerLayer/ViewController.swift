64eab14356af72b57e8757bd35cee987243c6836
Shigas <India1233@users.noreply.github.com>
2019-12-10 11:46:57 +0530

    initial commit...

--- ShadowCornerLayer/ViewController.swift
+++ ShadowCornerLayer/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var layerView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupLayer()
     }
 
+    private func setupLayer() {
+    
+        let layer = CAShapeLayer()
+        
+        let path = UIBezierPath(roundedRect: self.layerView.bounds, byRoundingCorners: [.bottomLeft, .bottomRight, .topLeft, .topRight], cornerRadii: CGSize(width: 10.0, height: 10.0))
+        layer.path = path.cgPath
+        layer.fillColor = UIColor.orange.cgColor
+//        layer.strokeColor = UIColor.white.cgColor
+        
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.shadowPath = path.cgPath
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 10.0
+        
+        self.layerView.layer.insertSublayer(layer, at: 0)
+        
+    }
 
 }
 
